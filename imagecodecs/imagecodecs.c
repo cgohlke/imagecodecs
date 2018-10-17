7be3b7b4457de7c87623fa796cf560bc093c7d78
Christoph Gohlke <cgohlke@uci.edu>
2018-10-17 11:19:47 -0700

    Update imagecodecs/imagecodecs.c

--- imagecodecs/imagecodecs.c
+++ imagecodecs/imagecodecs.c
@@ -6,10 +6,10 @@ Copyright (c) 2008-2018, The Regents of the University of California
 Produced at the Laboratory for Fluorescence Dynamics
 All rights reserved.
 
-Redistribution and use in src and binary forms, with or without
+Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 
-* Redistributions of src code must retain the above copyright
+* Redistributions of source code must retain the above copyright
   notice, this list of conditions and the following disclaimer.
 * Redistributions in binary form must reproduce the above copyright
   notice, this list of conditions and the following disclaimer in the
