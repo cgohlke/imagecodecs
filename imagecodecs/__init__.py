df6379243251cd58c698948a6b2cf373ed4fef64
Christoph Gohlke <cgohlke@uci.edu>
2018-11-01 14:15:01 -0700

    Update imagecodecs/__init__.py

--- imagecodecs/__init__.py
+++ imagecodecs/__init__.py
@@ -6,10 +6,19 @@ try:
     from ._imagecodecs import *
 except ImportError as error:
     import warnings
-    warnings.warn(
-        str(error) +
-        '\n\nThe _imagecodecs Cython extension module could not be found.\n'
-        'Using a fallback module with limited functionality and performance.\n'
-    )
+    warnings.warn("""
+
+%s
+
+*******************************************************************
+
+The _imagecodecs Cython extension module could not be loaded.
+Using a fallback module with limited functionality and performance.
+
+Install Windows binaries from https://pypi.org/project/imagecodecs/
+or build the _imagecodecs Cython extension module from source.
+
+*******************************************************************
+        """ % str(error))
     from .imagecodecs import __doc__, __version__
     from .imagecodecs import *
