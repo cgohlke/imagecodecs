412acd6d845b77e366d6cd68f1d966184008d493
Christoph Gohlke <cgohlke@uci.edu>
2019-02-22 11:30:19 -0800

    Update tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,16 @@
 # imagecodecs/conftest.py
 
 def pytest_report_header(config):
+    import os
+    print(__file__)
     try:
-        import imagecodecs
-        from imagecodecs import imagecodecs as imagecodecs_py
-        return 'versions C: %s\nversions Py: %s' % (
-            imagecodecs.version(), imagecodecs_py.version())
-    except Exception:
-        pass
+        if 'imagecodecs_lite' in os.path.abspath(__file__):
+            import imagecodecs_lite
+            return 'versions: %s' % (imagecodecs_lite.version())
+        else:
+            import imagecodecs
+            from imagecodecs import imagecodecs as imagecodecs_py
+            return 'versions C: %s\nversions Py: %s' % (
+                imagecodecs.version(), imagecodecs_py.version())
+    except Exception as e:
+        return 'pytest_report_header failed: %s' % str(e)
