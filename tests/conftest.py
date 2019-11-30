70195b813f833a0070581a421f74f4d168aa0f44
Christoph Gohlke <cgohlke@uci.edu>
2019-11-30 00:12:15 -0800

    Update tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,16 @@
 
 
 def pytest_report_header(config):
+    import sys
     import os
 
     try:
+        pyversion = 'Python %s' % sys.version.splitlines()[0]
         if 'imagecodecs_lite' in os.path.abspath(__file__):
             import imagecodecs_lite
 
-            return 'packagedir: %s\nversion: %s' % (
+            return '%s\packagedir: %s\nversion: %s' % (
+                pyversion,
                 imagecodecs_lite.__path__[0],
                 imagecodecs_lite.version(),
             )
@@ -17,7 +20,8 @@ def pytest_report_header(config):
             import imagecodecs
             from imagecodecs import imagecodecs as imagecodecs_py
 
-            return 'packagedir: %s\nversion: %s\nversions Py: %s' % (
+            return '%s\npackagedir: %s\nversion: %s\nversions Py: %s' % (
+                pyversion,
                 imagecodecs.__path__[0],
                 imagecodecs.version(),
                 imagecodecs_py.version(),
