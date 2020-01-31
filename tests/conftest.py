8308ff12f60af1fc7256df7305dd25100325b0a6
Christoph Gohlke <cgohlke@uci.edu>
2020-01-30 21:45:09 -0800

    Update tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,34 +1,19 @@
-# -*- coding: utf-8 -*-
-# imagecodecs/conftest.py
+# imagecodecs/tests/conftest.py
 
 
 def pytest_report_header(config):
     import sys
-    import os
 
     try:
-        pyversion = 'Python %s' % sys.version.splitlines()[0]
-        if (
-            'imagecodecs_lite' in os.getcwd() or
-            os.path.exists(os.path.join(os.path.dirname(__file__), '..',
-                                        'imagecodecs_lite'))
-        ):
-            import imagecodecs_lite
+        pyversion = f'Python {sys.version.splitlines()[0]}'
+        import imagecodecs
+        from imagecodecs import _imagecodecs
 
-            return '%s\npackagedir: %s\nversion: %s' % (
-                pyversion,
-                imagecodecs_lite.__path__[0],
-                imagecodecs_lite.version(),
+        return '{}\npackagedir: {}\nversion: {}\ndependencies: {}'.format(
+            pyversion,
+            imagecodecs.__path__[0],
+            imagecodecs.version(),
+            _imagecodecs.version(),
             )
-        else:
-            import imagecodecs
-            from imagecodecs import imagecodecs as imagecodecs_py
-
-            return '%s\npackagedir: %s\nversion: %s\nversions Py: %s' % (
-                pyversion,
-                imagecodecs.__path__[0],
-                imagecodecs.version(),
-                imagecodecs_py.version(),
-            )
-    except Exception as e:
-        return 'pytest_report_header failed: %s' % str(e)
+    except Exception as exc:
+        return f'pytest_report_header failed: {exc!s}'
