0e7d5ccc189908d8df3812e233bc06f2412735ae
Christoph Gohlke <cgohlke@uci.edu>
2021-03-31 10:46:09 -0700

    Update imagecodecs/_lzma.pyx

--- imagecodecs/_lzma.pyx
+++ imagecodecs/_lzma.pyx
@@ -1,4 +1,4 @@
-# _lzma.pyx
+# imagecodecs/_lzma.pyx
 # distutils: language = c
 # cython: language_level = 3
 # cython: boundscheck=False
@@ -35,19 +35,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-"""LZMA codec for the imagecodecs package.
-
-:Author:
-  `Christoph Gohlke <https://www.lfd.uci.edu/~gohlke/>`_
-
-:Organization:
-  Laboratory for Fluorescence Dynamics. University of California, Irvine
-
-:License: BSD 3-Clause
-
-:Version: 2020.12.22
-
-"""
+"""LZMA codec for the imagecodecs package."""
 
 __version__ = '2020.12.22'
 
