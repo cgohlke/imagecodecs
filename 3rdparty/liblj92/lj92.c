d05485bb7e554c1b0a7ffe56e25cb391e6cf7c51
Christoph Gohlke <cgohlke@uci.edu>
2021-07-30 20:40:03 -0700

    Update 3rdparty/liblj92/lj92.c

--- 3rdparty/liblj92/lj92.c
+++ 3rdparty/liblj92/lj92.c
@@ -85,7 +85,7 @@ typedef struct _ljp {
 static int find(ljp* self) {
     int ix = self->ix;
     u8* data = self->data;
-    while (data[ix] != 0xFF && ix<(self->datalen-1)) {
+    while (ix<(self->datalen-1) && data[ix] != 0xFF) {
         ix += 1;
     }
     ix += 2;
