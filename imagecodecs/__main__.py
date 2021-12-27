fccc3cbaedfd8809a69e923f302f2396c29fffe7
Christoph Gohlke <cgohlke@uci.edu>
2021-12-26 19:52:56 -0800

    Update imagecodecs/__main__.py

--- imagecodecs/__main__.py
+++ imagecodecs/__main__.py
@@ -1,6 +1,6 @@
 # imagecodecs/__main__.py
 
-# Copyright (c) 2019-2021, Christoph Gohlke
+# Copyright (c) 2019-2022, Christoph Gohlke
 # This source code is distributed under the BSD 3-Clause license.
 
 """Imagecodecs package command line script."""
