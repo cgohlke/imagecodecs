cf0c4f75f1e7e162033144841fcb93b575afc7c2
Christoph Gohlke <cgohlke@uci.edu>
2022-02-22 16:50:47 -0800

    Update imagecodecs/_blosc2.pyx

--- imagecodecs/_blosc2.pyx
+++ imagecodecs/_blosc2.pyx
@@ -6,7 +6,7 @@
 # cython: cdivision=True
 # cython: nonecheck=False
 
-# Copyright (c) 2021, Christoph Gohlke
+# Copyright (c) 2021-2022, Christoph Gohlke
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -37,7 +37,7 @@
 
 """Blosc2 codec for the imagecodecs package."""
 
-__version__ = '2021.11.11'
+__version__ = '2022.2.22'
 
 include '_shared.pxi'
 
