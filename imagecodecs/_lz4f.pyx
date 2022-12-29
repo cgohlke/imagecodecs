fdff831a256f2d7d31e70576e116a6bf9013dadc
Christoph Gohlke <cgohlke@cgohlke.com>
2022-12-29 12:17:33 -0800

    Update imagecodecs/_lz4f.pyx

--- imagecodecs/_lz4f.pyx
+++ imagecodecs/_lz4f.pyx
@@ -6,7 +6,7 @@
 # cython: cdivision=True
 # cython: nonecheck=False
 
-# Copyright (c) 2020-2022, Christoph Gohlke
+# Copyright (c) 2020-2023, Christoph Gohlke
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
