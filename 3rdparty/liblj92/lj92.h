1a054a7d4a38f95f0a8438108772cce9c2b6ea57
Christoph Gohlke <cgohlke@cgohlke.com>
2023-01-18 11:53:53 -0800

    Update 3rdparty/liblj92/lj92.h

--- 3rdparty/liblj92/lj92.h
+++ 3rdparty/liblj92/lj92.h
@@ -24,38 +24,58 @@ SOFTWARE.
 #ifndef LJ92_H
 #define LJ92_H
 
+#include <stdint.h>
+
+#define LJ92_VERSION "2023.1.23"
+
 enum LJ92_ERRORS {
     LJ92_ERROR_NONE = 0,
     LJ92_ERROR_CORRUPT = -1,
     LJ92_ERROR_NO_MEMORY = -2,
     LJ92_ERROR_BAD_HANDLE = -3,
     LJ92_ERROR_TOO_WIDE = -4,
+    LJ92_ERROR_ENCODER = -5
 };
 
-typedef struct _ljp* lj92;
+typedef struct _ljp *lj92;
 
 /* Parse a lossless JPEG (1992) structure returning
  * - a handle that can be used to decode the data
  * - width/height/bitdepth of the data
  * Returns status code.
- * If status == LJ92_ERROR_NONE, handle must be closed with lj92_close
+ * If status == LJ92_ERROR_NONE, handle must be closed with lj92_close.
  */
-int lj92_open(lj92* lj, // Return handle here
-              uint8_t* data,int datalen, // The encoded data
-              int* width,int* height,int* bitdepth,int* components); // Width, height, bitdepth and components
+int
+lj92_open(
+    lj92 *lj,  // return handle here
+    const uint8_t *data,
+    int datalen,
+    int *width,
+    int *height,
+    int *bitdepth,
+    int *components);
 
 /* Release a decoder object */
-void lj92_close(lj92 lj);
+void
+lj92_close(lj92 lj);
 
 /*
- * Decode previously opened lossless JPEG (1992) into a 2D tile of memory
- * Starting at target, write writeLength 16bit values, then skip 16bit skipLength value before writing again
- * If linearize is not NULL, use table at linearize to convert data values from output value to target value
- * Data is only correct if LJ92_ERROR_NONE is returned
+ * Decode previously opened lossless JPEG (1992) into a 2D tile of memory.
+ * Starting at target, write writeLength 16bit values, then skip 16bit
+ * skipLength value before writing again.
+ * If linearize is not NULL, use table at linearize to convert data values
+ * from output value to target value.
+ * Data is only correct if LJ92_ERROR_NONE is returned.
  */
-int lj92_decode(lj92 lj,
-                uint16_t* target, int writeLength, int skipLength, // The image is written to target as a tile
-                uint16_t* linearize, int linearizeLength); // If not null, linearize the data using this table
+int
+lj92_decode(
+    lj92 lj,
+    uint16_t *target,
+    int writeLength,
+    int skipLength,  // the image is written to target as a tile
+    const uint16_t *linearize,
+    int linearizeLength  // if not null, linearize the data using this table
+);
 
 /*
  * Encode a grayscale image supplied as 16bit values within the given bitdepth
@@ -63,8 +83,17 @@ int lj92_decode(lj92 lj,
  * Apply delinearization if given
  * Return the encoded lossless JPEG stream
  */
-int lj92_encode(uint16_t* image, int width, int height, int bitdepth, int components,
-                int readLength, int skipLength,
-                uint16_t* delinearize,int delinearizeLength,
-                uint8_t** encoded, int* encodedLength);
+int
+lj92_encode(
+    const uint16_t *image,
+    int width,
+    int height,
+    int bitdepth,
+    int components,
+    int readLength,
+    int skipLength,
+    const uint16_t *delinearize,
+    int delinearizeLength,
+    uint8_t **encoded,
+    int *encodedLength);
 #endif
