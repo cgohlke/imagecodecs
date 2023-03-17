126c22b563cee24515c6a6a70dd41beb06f8b56c
Christoph Gohlke <cgohlke@cgohlke.com>
2023-03-16 17:37:17 -0700

    Update imagecodecs/_jpeg8.pyx

--- imagecodecs/_jpeg8.pyx
+++ imagecodecs/_jpeg8.pyx
@@ -35,18 +35,13 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-"""JPEG 8-bit codec for the imagecodecs package."""
+"""JPEG codec for the imagecodecs package."""
 
-__version__ = '2022.8.8'
+__version__ = '2023.3.16'
 
 include '_shared.pxi'
 
-# DEF HAVE_LIBJPEG_TURBO = True
-
-IF HAVE_LIBJPEG_TURBO:
-    from libjpeg_turbo cimport *
-ELSE:
-    from libjpeg cimport *
+from libjpeg_turbo cimport *
 
 from cython.operator cimport dereference as deref
 
@@ -54,46 +49,45 @@ from libc.setjmp cimport setjmp, longjmp, jmp_buf
 
 
 class JPEG8:
-    """JPEG 8-bit Constants."""
+    """JPEG8 codec constants."""
+
+    available = True
 
     class CS(enum.IntEnum):
+        """JPEG8 codec color spaces."""
         UNKNOWN = JCS_UNKNOWN
         GRAYSCALE = JCS_GRAYSCALE
         RGB = JCS_RGB
         YCbCr = JCS_YCbCr
         CMYK = JCS_CMYK
         YCCK = JCS_YCCK
-        IF HAVE_LIBJPEG_TURBO:
-            EXT_RGB = JCS_EXT_RGB
-            EXT_RGBX = JCS_EXT_RGBX
-            EXT_BGR = JCS_EXT_BGR
-            EXT_BGRX = JCS_EXT_BGRX
-            EXT_XBGR = JCS_EXT_XBGR
-            EXT_XRGB = JCS_EXT_XRGB
-            EXT_RGBA = JCS_EXT_RGBA
-            EXT_BGRA = JCS_EXT_BGRA
-            EXT_ABGR = JCS_EXT_ABGR
-            EXT_ARGB = JCS_EXT_ARGB
-            RGB565 = JCS_RGB565
+        EXT_RGB = JCS_EXT_RGB
+        EXT_RGBX = JCS_EXT_RGBX
+        EXT_BGR = JCS_EXT_BGR
+        EXT_BGRX = JCS_EXT_BGRX
+        EXT_XBGR = JCS_EXT_XBGR
+        EXT_XRGB = JCS_EXT_XRGB
+        EXT_RGBA = JCS_EXT_RGBA
+        EXT_BGRA = JCS_EXT_BGRA
+        EXT_ABGR = JCS_EXT_ABGR
+        EXT_ARGB = JCS_EXT_ARGB
+        RGB565 = JCS_RGB565
 
 
 class Jpeg8Error(RuntimeError):
-    """JPEG 8-bit Exceptions."""
+    """JPEG8 codec exceptions."""
 
 
 def jpeg8_version():
-    """Return libjpeg library version string."""
-    IF HAVE_LIBJPEG_TURBO:
-        ver = str(LIBJPEG_TURBO_VERSION_NUMBER)
-        return 'libjpeg_turbo {}.{}.{}/{:.1f}'.format(
-            int(ver[:1]), int(ver[3:4]), int(ver[6:]), JPEG_LIB_VERSION / 10.0
-        )
-    ELSE:
-        return f'libjpeg {JPEG_LIB_VERSION_MAJOR}.{JPEG_LIB_VERSION_MINOR}'
+    """Return libjpeg-turbo library version string."""
+    ver = str(LIBJPEG_TURBO_VERSION_NUMBER)
+    return 'libjpeg_turbo {}.{}.{}/{:.1f}'.format(
+        int(ver[:1]), int(ver[3:4]), int(ver[5:]), JPEG_LIB_VERSION / 10.0
+    )
 
 
 def jpeg8_check(const uint8_t[::1] data):
-    """Return True if data likely contains a JPEG image."""
+    """Return whether data is JPEG encoded image."""
     sig = bytes(data[:10])
     return (
         sig[:4] == b'\xFF\xD8\xFF\xDB'
@@ -112,12 +106,13 @@ def jpeg8_encode(
     subsampling=None,
     optimize=None,
     smoothing=None,
-    numthreads=None,
+    lossless=None,
+    predictor=None,
+    bitspersample=None,
+    validate=None,  # for compatibility
     out=None
 ):
-    """Return JPEG 8-bit image from numpy array.
-
-    """
+    """Return JPEG encoded image."""
     cdef:
         numpy.ndarray src = numpy.asarray(data)
         const uint8_t[::1] dst  # must be const to write to bytes
@@ -128,7 +123,9 @@ def jpeg8_encode(
         int quality = _default_value(level, 95, 0, 100)
         my_error_mgr err
         jpeg_compress_struct cinfo
-        JSAMPROW rowpointer
+        JSAMPROW rowpointer8
+        J12SAMPROW rowpointer12
+        J16SAMPROW rowpointer16
         J_COLOR_SPACE in_color_space = JCS_UNKNOWN
         J_COLOR_SPACE jpeg_color_space = JCS_UNKNOWN
         unsigned long outsize = 0
@@ -138,9 +135,11 @@ def jpeg8_encode(
         int v_samp_factor = 0
         int smoothing_factor = _default_value(smoothing, -1, 0, 100)
         int optimize_coding = -1 if optimize is None else 1 if optimize else 0
+        int data_precision = 8 if src.dtype == numpy.uint8 else 12
+        int predictor_selection_value = 0
 
     if not (
-        src.dtype == numpy.uint8
+        (src.dtype == numpy.uint8 or src.dtype == numpy.uint16)
         and src.ndim in (2, 3)
         # src.nbytes < 2 ** 31 and  # limit to 2 GB
         and samples in (1, 3, 4)
@@ -148,6 +147,21 @@ def jpeg8_encode(
         and (src.ndim == 2 or src.strides[1] == samples * src.itemsize)
     ):
         raise ValueError('invalid data shape, strides, or dtype')
+
+    if lossless:
+        predictor_selection_value = 1 if predictor is None else predictor
+
+    if bitspersample is not None:
+        if (
+            bitspersample not in (8, 12, 16)
+            or src.itemsize == 1 and bitspersample > 8
+        ):
+            raise ValueError(f'invalid bitspersample {bitspersample}')
+        data_precision = bitspersample
+
+    # if validate and bitspersample == 12 and not _check_range(src):
+    #     # values larger than 12-bit cause segfault
+    #     raise ValueError('all data values must be < 4096')
 
     if colorspace is not None:
         in_color_space = _jcs_colorspace(colorspace)
@@ -221,41 +235,54 @@ def jpeg8_encode(
         cinfo.image_height = <JDIMENSION> src.shape[0]
         cinfo.image_width = <JDIMENSION> src.shape[1]
         cinfo.input_components = samples
+        cinfo.data_precision = data_precision
 
         if in_color_space != JCS_UNKNOWN:
             cinfo.in_color_space = in_color_space
 
         jpeg_set_defaults(&cinfo)
 
-        IF HAVE_LIBJPEG_TURBO:
-            jpeg_set_colorspace(&cinfo, jpeg_color_space)
-        ELSE:
-            if jpeg_color_space != JCS_UNKNOWN:
-                cinfo.jpeg_color_space = jpeg_color_space
+        jpeg_set_colorspace(&cinfo, jpeg_color_space)
         jpeg_mem_dest(&cinfo, &outbuffer, &outsize)  # must call after defaults
         jpeg_set_quality(&cinfo, quality, 1)
 
-        if smoothing_factor >= 0:
-            cinfo.smoothing_factor = smoothing_factor
-        if optimize_coding >= 0:
-            cinfo.optimize_coding = <boolean> optimize_coding
-        if h_samp_factor != 0:
-            cinfo.comp_info[0].h_samp_factor = h_samp_factor
-            cinfo.comp_info[0].v_samp_factor = v_samp_factor
-            cinfo.comp_info[1].h_samp_factor = 1
-            cinfo.comp_info[1].v_samp_factor = 1
-            cinfo.comp_info[2].h_samp_factor = 1
-            cinfo.comp_info[2].v_samp_factor = 1
+        if predictor_selection_value > 0:
+            jpeg_enable_lossless(&cinfo, predictor_selection_value, 0)
+        else:
+            if smoothing_factor >= 0:
+                cinfo.smoothing_factor = smoothing_factor
+            if optimize_coding >= 0:
+                cinfo.optimize_coding = <boolean> optimize_coding
+            if h_samp_factor != 0:
+                cinfo.comp_info[0].h_samp_factor = h_samp_factor
+                cinfo.comp_info[0].v_samp_factor = v_samp_factor
+                cinfo.comp_info[1].h_samp_factor = 1
+                cinfo.comp_info[1].v_samp_factor = 1
+                cinfo.comp_info[2].h_samp_factor = 1
+                cinfo.comp_info[2].v_samp_factor = 1
 
         # TODO: add option to use or return JPEG tables
 
         jpeg_start_compress(&cinfo, 1)
 
-        while cinfo.next_scanline < cinfo.image_height:
-            rowpointer = <JSAMPROW> (
-                <char*> src.data + cinfo.next_scanline * rowstride
-            )
-            jpeg_write_scanlines(&cinfo, &rowpointer, 1)
+        if cinfo.data_precision == 8:
+            while cinfo.next_scanline < cinfo.image_height:
+                rowpointer8 = <JSAMPROW> (
+                    <char*> src.data + cinfo.next_scanline * rowstride
+                )
+                jpeg_write_scanlines(&cinfo, &rowpointer8, 1)
+        elif cinfo.data_precision == 12:
+            while cinfo.next_scanline < cinfo.image_height:
+                rowpointer12 = <J12SAMPROW> (
+                    <char*> src.data + cinfo.next_scanline * rowstride
+                )
+                jpeg12_write_scanlines(&cinfo, &rowpointer12, 1)
+        elif cinfo.data_precision == 16:
+            while cinfo.next_scanline < cinfo.image_height:
+                rowpointer16 = <J16SAMPROW> (
+                    <char*> src.data + cinfo.next_scanline * rowstride
+                )
+                jpeg16_write_scanlines(&cinfo, &rowpointer16, 1)
 
         jpeg_finish_compress(&cinfo)
         jpeg_destroy_compress(&cinfo)
@@ -274,17 +301,13 @@ def jpeg8_encode(
 
 def jpeg8_decode(
     data,
-    index=None,
     tables=None,
     colorspace=None,
     outcolorspace=None,
     shape=None,
-    numthreads=None,
     out=None
 ):
-    """Decode JPEG 8-bit image to numpy array.
-
-    """
+    """Return decoded JPEG image."""
     cdef:
         numpy.ndarray dst
         const uint8_t[::1] src = data
@@ -296,7 +319,9 @@ def jpeg8_decode(
         int numlines
         my_error_mgr err
         jpeg_decompress_struct cinfo
-        JSAMPROW rowpointer
+        JSAMPROW rowpointer8
+        J12SAMPROW rowpointer12
+        J16SAMPROW rowpointer16
         J_COLOR_SPACE jpeg_color_space
         J_COLOR_SPACE out_color_space
         JDIMENSION width = 0
@@ -364,24 +389,40 @@ def jpeg8_decode(
 
         with gil:
             # if (cinfo.output_components not in
-            #         _jcs_colorspace_samples(out_color_space)):
+            #     _jcs_colorspace_samples(out_color_space)):
             #     raise ValueError('invalid output shape')
 
             shape = cinfo.output_height, cinfo.output_width
             if cinfo.output_components > 1:
                 shape += cinfo.output_components,
 
-            out = _create_array(out, shape, numpy.uint8)  # TODO: allow strides
+            # TODO: allow strides
+
+            if cinfo.data_precision <= 8:
+                out = _create_array(out, shape, numpy.uint8)
+            else:
+                out = _create_array(out, shape, numpy.uint16)
             dst = out
             dstsize = dst.nbytes
-            rowstride = dst.strides[0]
+            rowstride = dst.strides[0] // dst.itemsize
 
         memset(<void*> dst.data, 0, dstsize)
-        rowpointer = <JSAMPROW> dst.data
-        while cinfo.output_scanline < cinfo.output_height:
-            jpeg_read_scanlines(&cinfo, &rowpointer, 1)
-            rowpointer += rowstride
-
+        if cinfo.data_precision <= 8:
+            rowpointer8 = <JSAMPROW> dst.data
+            while cinfo.output_scanline < cinfo.output_height:
+                jpeg_read_scanlines(&cinfo, &rowpointer8, 1)
+                rowpointer8 += rowstride
+        elif cinfo.data_precision == 12:
+            rowpointer12 = <J12SAMPROW> dst.data
+            while cinfo.output_scanline < cinfo.output_height:
+                jpeg12_read_scanlines(&cinfo, &rowpointer12, 1)
+                rowpointer12 += rowstride
+        else:
+            # elif cinfo.data_precision == 16:
+            rowpointer16 = <J16SAMPROW> dst.data
+            while cinfo.output_scanline < cinfo.output_height:
+                jpeg16_read_scanlines(&cinfo, &rowpointer16, 1)
+                rowpointer16 += rowstride
         jpeg_finish_decompress(&cinfo)
         jpeg_destroy_decompress(&cinfo)
 
@@ -393,106 +434,102 @@ ctypedef struct my_error_mgr:
     jmp_buf setjmp_buffer
 
 
-cdef void my_error_exit(jpeg_common_struct* cinfo) nogil:
+cdef void my_error_exit(jpeg_common_struct* cinfo) noexcept nogil:
     cdef:
         my_error_mgr* error = <my_error_mgr*> deref(cinfo).err
 
     longjmp(deref(error).setjmp_buffer, 1)
 
 
-cdef void my_output_message(jpeg_common_struct* cinfo) nogil:
+cdef void my_output_message(jpeg_common_struct* cinfo) noexcept nogil:
     pass
 
 
-def _jcs_colorspace(colorspace):
+cdef _jcs_colorspace(colorspace):
     """Return JCS colorspace value from user input."""
     if isinstance(colorspace, str):
         colorspace = colorspace.upper()
-    IF HAVE_LIBJPEG_TURBO:
-        jcs = {
-            None: JCS_UNKNOWN,
-            'UNKNOWN': JCS_UNKNOWN,
-            'GRAY': JCS_GRAYSCALE,
-            'GRAYSCALE': JCS_GRAYSCALE,
-            'MINISWHITE': JCS_GRAYSCALE,
-            'MINISBLACK': JCS_GRAYSCALE,
-            'RGB': JCS_RGB,
-            'CMYK': JCS_CMYK,
-            'SEPARATED': JCS_CMYK,
-            'YCCK': JCS_YCCK,
-            'YCBCR': JCS_YCbCr,
-            'RGBA': JCS_EXT_RGBA,
-            JCS_UNKNOWN: JCS_UNKNOWN,
-            JCS_GRAYSCALE: JCS_GRAYSCALE,
-            JCS_RGB: JCS_RGB,
-            JCS_YCbCr: JCS_YCbCr,
-            JCS_CMYK: JCS_CMYK,
-            JCS_YCCK: JCS_YCCK,
-            JCS_EXT_RGB: JCS_EXT_RGB,
-            JCS_EXT_RGBX: JCS_EXT_RGBX,
-            JCS_EXT_BGR: JCS_EXT_BGR,
-            JCS_EXT_BGRX: JCS_EXT_BGRX,
-            JCS_EXT_XBGR: JCS_EXT_XBGR,
-            JCS_EXT_XRGB: JCS_EXT_XRGB,
-            JCS_EXT_RGBA: JCS_EXT_RGBA,
-            JCS_EXT_BGRA: JCS_EXT_BGRA,
-            JCS_EXT_ABGR: JCS_EXT_ABGR,
-            JCS_EXT_ARGB: JCS_EXT_ARGB,
-            JCS_RGB565: JCS_RGB565,
-        }
-    ELSE:
-        jcs = {
-            None: JCS_UNKNOWN,
-            'UNKNOWN': JCS_UNKNOWN,
-            'GRAY': JCS_GRAYSCALE,
-            'GRAYSCALE': JCS_GRAYSCALE,
-            'MINISWHITE': JCS_GRAYSCALE,
-            'MINISBLACK': JCS_GRAYSCALE,
-            'RGB': JCS_RGB,
-            'CMYK': JCS_CMYK,
-            'YCCK': JCS_YCCK,
-            'YCBCR': JCS_YCbCr,
-            JCS_UNKNOWN: JCS_UNKNOWN,
-            JCS_GRAYSCALE: JCS_GRAYSCALE,
-            JCS_RGB: JCS_RGB,
-            JCS_YCbCr: JCS_YCbCr,
-            JCS_CMYK: JCS_CMYK,
-            JCS_YCCK: JCS_YCCK,
-        }
-    return jcs.get(colorspace, JCS_UNKNOWN)
+    return {
+        None: JCS_UNKNOWN,
+        'UNKNOWN': JCS_UNKNOWN,
+        'GRAY': JCS_GRAYSCALE,
+        'GRAYSCALE': JCS_GRAYSCALE,
+        'MINISWHITE': JCS_GRAYSCALE,
+        'MINISBLACK': JCS_GRAYSCALE,
+        'RGB': JCS_RGB,
+        'CMYK': JCS_CMYK,
+        'SEPARATED': JCS_CMYK,
+        'YCCK': JCS_YCCK,
+        'YCBCR': JCS_YCbCr,
+        'RGBA': JCS_EXT_RGBA,
+        JCS_UNKNOWN: JCS_UNKNOWN,
+        JCS_GRAYSCALE: JCS_GRAYSCALE,
+        JCS_RGB: JCS_RGB,
+        JCS_YCbCr: JCS_YCbCr,
+        JCS_CMYK: JCS_CMYK,
+        JCS_YCCK: JCS_YCCK,
+        JCS_EXT_RGB: JCS_EXT_RGB,
+        JCS_EXT_RGBX: JCS_EXT_RGBX,
+        JCS_EXT_BGR: JCS_EXT_BGR,
+        JCS_EXT_BGRX: JCS_EXT_BGRX,
+        JCS_EXT_XBGR: JCS_EXT_XBGR,
+        JCS_EXT_XRGB: JCS_EXT_XRGB,
+        JCS_EXT_RGBA: JCS_EXT_RGBA,
+        JCS_EXT_BGRA: JCS_EXT_BGRA,
+        JCS_EXT_ABGR: JCS_EXT_ABGR,
+        JCS_EXT_ARGB: JCS_EXT_ARGB,
+        JCS_RGB565: JCS_RGB565,
+    }.get(colorspace, JCS_UNKNOWN)
 
 
-def _jcs_colorspace_samples(colorspace):
+cdef _jcs_colorspace_samples(colorspace):
     """Return expected number of samples in colorspace."""
     three = (3,)
     four = (4,)
-    IF HAVE_LIBJPEG_TURBO:
-        jcs = {
-            JCS_UNKNOWN: (1, 2, 3, 4),
-            JCS_GRAYSCALE: (1,),
-            JCS_RGB: three,
-            JCS_YCbCr: three,
-            JCS_CMYK: four,
-            JCS_YCCK: four,
-            JCS_EXT_RGB: three,
-            JCS_EXT_RGBX: four,
-            JCS_EXT_BGR: three,
-            JCS_EXT_BGRX: four,
-            JCS_EXT_XBGR: four,
-            JCS_EXT_XRGB: four,
-            JCS_EXT_RGBA: four,
-            JCS_EXT_BGRA: four,
-            JCS_EXT_ABGR: four,
-            JCS_EXT_ARGB: four,
-            JCS_RGB565: three,
-        }
-    ELSE:
-        jcs = {
-            JCS_UNKNOWN: (1, 2, 3, 4),
-            JCS_GRAYSCALE: (1,),
-            JCS_RGB: three,
-            JCS_YCbCr: three,
-            JCS_CMYK: four,
-            JCS_YCCK: four,
-        }
-    return jcs[colorspace]
+    return {
+        JCS_UNKNOWN: (1, 2, 3, 4),
+        JCS_GRAYSCALE: (1,),
+        JCS_RGB: three,
+        JCS_YCbCr: three,
+        JCS_CMYK: four,
+        JCS_YCCK: four,
+        JCS_EXT_RGB: three,
+        JCS_EXT_RGBX: four,
+        JCS_EXT_BGR: three,
+        JCS_EXT_BGRX: four,
+        JCS_EXT_XBGR: four,
+        JCS_EXT_XRGB: four,
+        JCS_EXT_RGBA: four,
+        JCS_EXT_BGRA: four,
+        JCS_EXT_ABGR: four,
+        JCS_EXT_ARGB: four,
+        JCS_RGB565: three,
+    }[colorspace]
+
+
+cdef bint _check_range(numpy.ndarray data, uint16_t upper=4095):
+    """Return if all values are below upper."""
+    cdef:
+        numpy.flatiter srciter
+        uint8_t* srcptr = NULL
+        ssize_t srcsize = 0
+        ssize_t srcstride = 0
+        ssize_t i
+        int axis = -1
+        bint ret = True
+
+    srciter = numpy.PyArray_IterAllButAxis(data, &axis)
+    srcsize = data.shape[axis]
+    srcstride = data.strides[axis]
+
+    with nogil:
+        while ret and numpy.PyArray_ITER_NOTDONE(srciter):
+            srcptr = <uint8_t*> numpy.PyArray_ITER_DATA(srciter)
+            for i in range(srcsize):
+                if (<uint16_t*> srcptr)[0] > upper:
+                    ret = False
+                    break
+                srcptr += srcstride
+            numpy.PyArray_ITER_NEXT(srciter)
+
+    return ret
