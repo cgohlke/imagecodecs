ab97a66240c9be055ebd216871b53957827daf36
Christoph Gohlke <cgohlke@cgohlke.com>
2023-12-27 20:28:36 -0800

    Update imagecodecs/_zlibng.pyx

--- imagecodecs/_zlibng.pyx
+++ imagecodecs/_zlibng.pyx
@@ -6,7 +6,7 @@
 # cython: cdivision=True
 # cython: nonecheck=False
 
-# Copyright (c) 2021-2023, Christoph Gohlke
+# Copyright (c) 2021-2024, Christoph Gohlke
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -36,8 +36,6 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 """Zlib-ng codec for the imagecodecs package."""
-
-__version__ = '2023.8.12'
 
 include '_shared.pxi'
 
@@ -310,7 +308,7 @@ ctypedef struct output_t:
     int owner
 
 
-cdef output_t* output_new(uint8_t* data, size_t size) nogil:
+cdef output_t* output_new(uint8_t* data, size_t size) noexcept nogil:
     """Return new output."""
     cdef:
         output_t* output = <output_t*> malloc(sizeof(output_t))
@@ -332,7 +330,7 @@ cdef output_t* output_new(uint8_t* data, size_t size) nogil:
     return output
 
 
-cdef void output_del(output_t* output) nogil:
+cdef void output_del(output_t* output) noexcept nogil:
     """Free output."""
     if output != NULL:
         if output.owner != 0:
@@ -340,7 +338,7 @@ cdef void output_del(output_t* output) nogil:
         free(output)
 
 
-cdef int output_seek(output_t* output, size_t pos) nogil:
+cdef int output_seek(output_t* output, size_t pos) noexcept nogil:
     """Seek output to position."""
     if output == NULL or pos > output.size:
         return 0
@@ -350,7 +348,7 @@ cdef int output_seek(output_t* output, size_t pos) nogil:
     return 1
 
 
-cdef int output_resize(output_t* output, size_t newsize) nogil:
+cdef int output_resize(output_t* output, size_t newsize) noexcept nogil:
     """Resize output."""
     cdef:
         uint8_t* tmp
