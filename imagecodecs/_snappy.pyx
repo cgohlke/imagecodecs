5988b44076a9140d74a465b3c2a8c0f50b097d12
Christoph Gohlke <cgohlke@cgohlke.com>
2024-04-05 11:09:09 -0700

    Update imagecodecs/_snappy.pyx

--- imagecodecs/_snappy.pyx
+++ imagecodecs/_snappy.pyx
@@ -65,7 +65,7 @@ def snappy_version():
     """Return Snappy library version string."""
     # TODO: use version from header when available
     # TODO: SNAPPY_VERSION is defined in snappy-stubs-public.h C++ header
-    return 'snappy 1.1.x'
+    return 'snappy 1.2.x'
 
 
 def snappy_check(arg):
