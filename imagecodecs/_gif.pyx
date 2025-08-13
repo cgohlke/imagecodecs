d6c26aa3499c440154065982fb0b3b86a1d3d0a0
Christoph Gohlke <cgohlke@cgohlke.com>
2025-08-13 12:23:57 -0700

    Update imagecodecs/_gif.pyx

--- imagecodecs/_gif.pyx
+++ imagecodecs/_gif.pyx
@@ -1,10 +1,10 @@
 # imagecodecs/_gif.pyx
 # distutils: language = c
 # cython: language_level = 3
-# cython: boundscheck=False
-# cython: wraparound=False
-# cython: cdivision=True
-# cython: nonecheck=False
+# cython: boundscheck = False
+# cython: wraparound = False
+# cython: cdivision = True
+# cython: nonecheck = False
 # cython: freethreading_compatible = True
 
 # Copyright (c) 2019-2025, Christoph Gohlke
